6e82701e673563e4fa5dc916fb7cedde31bacdbe
Lu Hernandez <guicholh@gmail.com>
2021-01-06 15:56:37 -0600

    working on dropdown for states on registering

--- src/components/Register.js
+++ src/components/Register.js
@@ -7,9 +7,77 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  makeStyles,
 } from "@material-ui/core";
 const Register = () => {
   const [open, setOpen] = useState(false);
+
+  const states = [
+    { name: "Alabama", code: "AL" },
+    { name: "Alaska", code: "AK" },
+    { name: "Arizona", code: "AZ" },
+    { name: "Arkansas", code: "AR" },
+    { name: "California", code: "CA" },
+    { name: "Colorado", code: "CO" },
+    { name: "Conneticut", code: "CT" },
+    { name: "Delaware", code: "DE" },
+    { name: "Florida", code: "FL" },
+    { name: "Georgia", code: "GA" },
+    { name: "Hawaii", code: "HI" },
+    { name: "Idaho", code: "ID" },
+    { name: "Illinois", code: "IL" },
+    { name: "Indiana", code: "IN" },
+    { name: "Iowa", code: "IA" },
+    { name: "Kansas", code: "KS" },
+    { name: "Kentucky", code: "KS" },
+    { name: "Lousiana", code: "LA" },
+    { name: "Maine", code: "ME" },
+    { name: "Maryland", code: "MD" },
+    { name: "Massachusetts", code: "MA" },
+    { name: "Michigan", code: "MI" },
+    { name: "Minnesota", code: "MN" },
+    { name: "Mississippi", code: "MS" },
+    { name: "Missouri", code: "MO" },
+    { name: "Montana", code: "MT" },
+    { name: "Nebraska", code: "NE" },
+    { name: "Nevada", code: "NV" },
+    { name: "New Hampshire", code: "NH" },
+    { name: "New Jersey", code: "NJ" },
+    { name: "New Mexico", code: "NM" },
+    { name: "New York", code: "NY" },
+    { name: "North Carolina", code: "NC" },
+    { name: "North Dakota", code: "ND" },
+    { name: "Ohio", code: "OH" },
+    { name: "Oklahoma", code: "OK" },
+    { name: "Oregon", code: "OR" },
+    { name: "Pennsylvania", code: "PA" },
+    { name: "Rhode Island", code: "RI" },
+    { name: "South Carolina", code: "SC" },
+    { name: "South Dakota", code: "SD" },
+    { name: "Tennessee", code: "TN" },
+    { name: "Texas", code: "TX" },
+    { name: "Utah", code: "UT" },
+    { name: "Vermont", code: "VT" },
+    { name: "Virginia", code: "VA" },
+    { name: "Washington", code: "WA" },
+    { name: "West Virginia", code: "WV" },
+    { name: "Wisconsin", code: "WI" },
+    { name: "Wyoming", code: "WY" },
+  ];
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      "& .MuiTextField-root": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
+    },
+  }));
+
+  const MultiLineTextField = () => {
+    const classes = useStyles();
+    const [stateCode, setStateCode] = useState("AL");
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -45,6 +113,7 @@ const Register = () => {
           <TextField
             autoFocus
             margin="dense"
+            type="password"
             label="Password"
             required={true}
             fullWidth
@@ -52,14 +121,14 @@ const Register = () => {
           <TextField
             autoFocus
             margin="dense"
-            label="City"
+            label="State"
             required={true}
             fullWidth
           />
           <TextField
             autoFocus
             margin="dense"
-            label="State"
+            label="City"
             required={true}
             fullWidth
           />
