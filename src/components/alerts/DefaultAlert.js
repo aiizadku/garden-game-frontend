e98989fc44d16cf902e844d2f79102b05b936dce
zjthom3 <zigzag20705@gmail.com>
2021-01-07 18:00:54 -0500

    you can create an account!

---
+++ src/components/alerts/DefaultAlert.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+const DefaultAlert = (props) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Alert severity={props.type}>
+        
+        <strong>{props.message}</strong>
+      </Alert>
+    </div>
+  );
+};
+
+export default DefaultAlert;
