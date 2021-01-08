568aa5b2649f6bf8b066a4b4b56a241ec3b8cb7d
jmrideout <jmrideout@hotmail.com>
2021-01-08 10:32:52 -0600

    Installed gsap. Created Garden, GardenPlot, Plant, Sky and Ground componenets. Added images and image selector function.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LoginPage from "./pages/LoginPage.js"
+import GardenPage from "./pages/GardenPage";
 
 function App() {
   return (
     <div className="App">
       <LoginPage />
+      {/*<GardenPage />*/}
     </div>
   );
 }
---
+++ src/components/Garden/Garden.js
@@ -0,0 +1,107 @@
+import { makeStyles } from '@material-ui/core';
+import GardenPlot from './GardenPlot';
+
+// [row][col]
+const exampleData = {
+  "plants": [
+    [
+      {id:0, growthPercent: 0},
+      {id:1, growthPercent: 0},
+      {id:2, growthPercent: 0},
+      {id:3, growthPercent: 0},
+      {id:4, growthPercent: 0}
+    ],
+    [
+      {id:0, growthPercent: 20},
+      {id:1, growthPercent: 20},
+      {id:2, growthPercent: 20},
+      {id:3, growthPercent: 20},
+      {id:4, growthPercent: 20}
+    ],
+    [
+      {id:0, growthPercent: 50},
+      {id:1, growthPercent: 50},
+      {id:2, growthPercent: 50},
+      {id:3, growthPercent: 50},
+      {id:4, growthPercent: 50}
+    ],
+    [
+      {id:0, growthPercent: 100},
+      {id:1, growthPercent: 100},
+      {id:2, growthPercent: 100},
+      {id:3, growthPercent: 100},
+      {id:4, growthPercent: 100}
+    ]
+  ]
+};
+
+
+
+const useStyles = makeStyles({
+  flexContainer: {
+    display: "inline-flex",
+    flexDirection: "column",
+    flexWrap: "nowrap",
+    justifyContent: "center",
+    alignContent: "center",
+    borderRadius: 10,
+    backgroundImage: "repeating-linear-gradient(to bottom, saddlebrown 0px 20px, sienna 20px 40px)",
+    border: "4px solid #7B3503"
+  },
+  flexRow: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  flexPlot: {
+    width: 100,
+    height: 100,
+    position: 'relative',
+    border: "2px solid white"
+  },
+  centered: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  }
+});
+
+const Garden = (props) => {
+  const makeGardenGrid = (rows, cols) => {
+    let gardenPlots = [];
+    for (let r = 0; r < rows; r++) {
+      let gardenRow = [];
+      for (let c = 0; c < cols; c++) {
+        gardenRow.push(
+          <div classname={classes.flexPlot}>
+            <GardenPlot plantId={exampleData["plants"][r][c]["id"]} id={`plot${r}-${c}`} isPlant={true} growthPercent={exampleData["plants"][r][c]["growthPercent"]}/>
+          </div>
+        );
+      }
+      gardenPlots.push(
+        <div className={classes.flexRow}>
+          {gardenRow}
+        </div>
+      );
+    }
+    return (
+      <div className={classes.flexContainer}>
+        {gardenPlots}
+      </div>
+    );
+  }
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.centered}>
+      {makeGardenGrid(4, 5)}
+    </div>
+  );
+}
+
+export default Garden;
+
