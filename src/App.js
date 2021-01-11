416b835b12b86c99bced093a85681c9530959fe1
Aiizad Kulchoroeva <kulchoroeva@gmail.com>
2021-01-11 12:48:15 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -4,54 +4,21 @@ import "./App.css";
 import LoginPage from "./pages/LoginPage.js";
 import HomePage from "./pages/HomePage.js";
 import UserContextProvider from './contexts/UserContext';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userState: "",
-      userCity: "",
-    };
-  }
-  componentWillMount() {
-    fetch(
-      "https://find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com/iplocation?apikey=873dbe322aea47f89dcf729dcc8f60e8",
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-key":
-            "9d1fd4c712msh8524d11d1eeb2b0p1de58ajsn856245a097d9",
-          "x-rapidapi-host":
-            "find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com",
-        },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((location) => {
-        this.setState({ userState: location.state, userCity: location.city });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-
-    
-  }
-
+  
   render() {
     return (
       <div className="App">
-
         <UserContextProvider>
-        <LoginPage
-          userState={this.state.userState}
-          userCity={this.state.userCity}
-        />
-        {/* <HomePage /> */}
+          <BrowserRouter>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/garden" component={GardenPage} />
+          </BrowserRouter>
         </UserContextProvider>
-        
-
       </div>
     );
   }
