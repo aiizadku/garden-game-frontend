7b5127427f20aaffb676e15799e449db5e779ebd
Lu Hernandez <guicholh@gmail.com>
2021-01-18 15:31:31 -0600

    merged newest changes from main, weather will change skybox

--- src/components/Garden/Garden.js
+++ src/components/Garden/Garden.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import GardenPlot from './GardenPlot';
 import { harvestPlant, loadGarden, getPlantDetail } from "../../api/GameApi";
